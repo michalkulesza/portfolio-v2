4af933e7f1b12e467b13a48a21824778773ece09
Michal <kulesza.michal@gmail.com>
2020-11-18 22:12:19 +0100

    add: shadows to snippets

--- src/styles/snippets.ts
+++ src/styles/snippets.ts
@@ -4,4 +4,12 @@ export const Colors = {
 	grey: "#747474",
 	lightGrey: "#F6F6F6",
 	white: "#f2f2f2",
+	yellow: "#E6C271",
+	green: "#2EB087",
+};
+
+export const Shadow = {
+	small: "0 10px 20px -5px rgba(50, 50, 93, 0.25), 0 8px 11px -8px rgba(0, 0, 0, 0.3);",
+	medium: "0 30px 30px -9px rgba(50,50,93,0.25), 0 10px 26px -5px rgba(0,0,0,0.3);",
+	large: "0 30px 60px -12px rgba(50,50,93,0.25), 0 18px 36px -18px rgba(0,0,0,0.3);",
 };
