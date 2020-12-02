4df3323ce26276565f92665be0d91852315d7082
Michal <kulesza.michal@gmail.com>
2020-12-02 12:49:06 +0100

    fix: changed email

--- src/fixtures/footer.ts
+++ src/fixtures/footer.ts
@@ -3,7 +3,7 @@ export const footerData = {
 	title: "Want to know more?",
 	text: `If you have any more questions about me, my projects or anything related really, 
    please send me a message and I will get back to you as soon as possible.`,
-	email: "contact@michalkulesza.me",
+	email: "contact@mkulesza.com",
 	footer: `© Copyright 2020 Michal Kulesza.
    All rights reserved.`,
 };
